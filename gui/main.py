1a0b072fcc20ed58baf50404e8914dd1465492f1
nguyenhuyenag <42534837+nguyenhuyenag@users.noreply.github.com>
2025-04-09 21:25:35 +0700

    #

--- gui/main.py
+++ gui/main.py
@@ -1,35 +1,37 @@
 import tkinter as tk
 from tkinter import ttk, scrolledtext, messagebox
-from sympy import symbols
-from itertools import combinations_with_replacement
+
+import sympy as sp
 
 
-def get_terms(variables, max_deg):
-    vars = symbols(variables)
-    terms = [1]  # Hằng số 1
-
-    for deg in range(1, max_deg + 1):
-        for combo in combinations_with_replacement(vars, deg):
-            term = 1
-            for var in combo:
-                term *= var
-            terms.append(term)
-
-    return terms
+# Hàm lấy dữ liệu từ ô Input
+def get_input():
+    return sp.simplify(input_poly.get(1.0, tk.END).strip())
 
 
-def generate_terms(vars):
+# Hàm chèn kết quả vào ô Output
+def output(poly):
+    output_text.delete(1.0, tk.END)  # Xóa nội dung cũ
+    output_text.insert(tk.END, str(poly))  # Chèn kết quả mới
+
+
+def pqr():
     try:
-        max_deg = int(input_deg.get("1.0", tk.END).strip())
-        if max_deg < 0:
-            raise ValueError("Bậc phải ≥ 0")
-
-        terms = get_terms(vars, max_deg)
-        output_text.delete("1.0", tk.END)
-        output_text.insert(tk.END, "\n".join(map(str, terms)))
-
+        poly = get_input()
+        # TODO: xử lý đa thức ở đây nếu cần
+        result = f"{poly}"
+        output(result)
     except Exception as e:
-        messagebox.showerror("Lỗi", str(e))
+        messagebox.showerror("Error", str(e))
+
+
+def uvw():
+    try:
+        poly = get_input()
+        result = f"{poly}"
+        output(result)
+    except Exception as e:
+        messagebox.showerror("Error", str(e))
 
 
 # Tạo cửa sổ
@@ -48,9 +50,9 @@ left_frame.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
 # Ô nhập bậc
 h = 8
 ttk.Label(left_frame, text="Input:").pack(anchor=tk.W)
-input_deg = scrolledtext.ScrolledText(left_frame, height=h, wrap=tk.WORD)
-input_deg.pack(fill=tk.X, pady=5)
-input_deg.insert(tk.END, "2")  # default value
+input_poly = scrolledtext.ScrolledText(left_frame, height=h, wrap=tk.WORD)
+input_poly.pack(fill=tk.X, pady=5)
+input_poly.insert(tk.END, "a^2+b^2+c^2")  # default value
 
 # Ô kết quả
 ttk.Label(left_frame, text="Output:").pack(anchor=tk.W)
@@ -66,13 +68,10 @@ right_frame = ttk.Frame(main_frame, width=100)
 right_frame.pack(side=tk.LEFT, fill=tk.Y)
 
 # Các nút xếp DỌC từ trên xuống
-btn_pqr = ttk.Button(right_frame, text="pqr", width=10, command=lambda: generate_terms("p q r"))
+btn_pqr = ttk.Button(right_frame, text="pqr", width=10, command=pqr)
 btn_pqr.pack(pady=10)
 
-btn_uvw = ttk.Button(right_frame, text="uvw", width=10, command=lambda: generate_terms("u v w"))
+btn_uvw = ttk.Button(right_frame, text="uvw", width=10, command=uvw)
 btn_uvw.pack(pady=10)
-
-# btn_pRr = ttk.Button(right_frame, text="pRr", width=10, command=lambda: generate_terms("p R r"))
-# btn_pRr.pack(pady=10)
 
 root.mainloop()
