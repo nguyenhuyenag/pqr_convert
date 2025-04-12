067839e7ae2f7afa6ea4a5be41e7b4f7f00c0367
nguyenhuyenag <42534837+nguyenhuyenag@users.noreply.github.com>
2025-04-12 22:53:28 +0700

    Add docstring to polynomial.py

--- core/polynomial.py
+++ core/polynomial.py
@@ -5,11 +5,22 @@ import sympy as sp
 from sympy import Poly, Expr, S, Mul, Symbol, IndexedBase
 
 _coeff_counter = 1
-"""Global counter for indexing coefficients in generated polynomials."""
+"""A global counter used to assign unique indices to coefficients."""
 
 
 def monomials(symbols: Union[List[str], List[Symbol]], degree: int, is_homogeneous: bool = False) -> List[Expr]:
-    """ Generate a list of monomials up to a given total degree using specified variables. """
+    """
+        Generate a list of monomials up to a given total degree using specified variables.
+
+        Example:
+        > monomials(['a', 'b', 'c'], 2)
+
+            [1, a*b, a**2, a*c, a, b**2, b*c, c**2, c, b]
+
+        > monomials(['a', 'b', 'c'], 2, is_homogeneous=True)
+
+            [a*b, b**2, c**2, b*c, a**2, a*c]
+    """
     symbols = [sp.symbols(x) if isinstance(x, str) else x for x in symbols]
 
     monomial_list = set() if is_homogeneous else {S.One}
@@ -26,7 +37,15 @@ def monomials(symbols: Union[List[str], List[Symbol]], degree: int, is_homogeneo
 
 
 def generate_polynomial(monomial_list: List[Expr], coeff_name: str = 'm') -> Tuple[Poly, List[Symbol]]:
-    """Generate a symbolic polynomial with indexed coefficients from a list of monomials."""
+    """
+        Generate a symbolic polynomial with indexed coefficients from a list of monomials.
+
+        Example:
+        > mons = monomials(['x', 'y'], 2)
+        > poly = generate_polynomial(mons, 'm')
+
+            x**2*m[5] + x*y*m[2] + x*m[6] + y**2*m[3] + y*m[4] + m[1]
+    """
     global _coeff_counter
 
     idx = IndexedBase(coeff_name)
@@ -48,3 +67,9 @@ def generate_polynomial(monomial_list: List[Expr], coeff_name: str = 'm') -> Tup
 def poly_zero(poly: Poly) -> Set[Expr]:
     """Extract the set of constant terms (coefficients) from a sympy polynomial."""
     return set(poly.as_dict().values())
+
+
+mons = monomials(['x', 'y'], 2)
+poly = generate_polynomial(mons, 'm')
+print(poly.as_expr())
+# Unresolved attribute reference 'as_expr' for class '(Poly, list)'
