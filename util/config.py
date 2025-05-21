ae1a2d996e024ba7ff9a48d06d826686e1a02854
nguyenhuyenag <42534837+nguyenhuyenag@users.noreply.github.com>
2025-05-21 16:13:08 +0700

    Update version

--- util/config.py
+++ util/config.py
@@ -1,4 +1,8 @@
-version = '18-04-2025'
+from datetime import datetime
+
+version = '21-04-2025'
+# version = datetime.today().strftime('%d-%m-%Y')
+
 latex_view = "https://latex.codecogs.com/svg.image"
 home_url = 'https://github.com/nguyenhuyenag/pqr_convert'
 
